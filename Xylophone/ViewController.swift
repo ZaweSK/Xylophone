821b97f2cbbcf5b3f3c948fc3cb4388856b733af
Peter Sevcik <psevcikbn@gmail.com>
2019-03-28 09:02:36 +0100

    Complete Xylophone

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var selectedSoundFileName: String = ""
+    var audioPlayer: AVAudioPlayer!
+    let soundFiles = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    
+      
     }
-
+    
+    func playSound(){
+        let url = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+        }catch {
+            print(error)
+        }
+        audioPlayer.play()
+    }
+    
+    
+    @IBAction func notePressed(_ sender: UIButton){
+        selectedSoundFileName = soundFiles[sender.tag - 1]
+        playSound()
+    }
 
 }
 
